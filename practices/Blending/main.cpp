919141b3a937f4fb33ca95092080b14994ac7b30
Muru Chen <muruc@andrew.cmu.edu>
2022-02-04 23:19:40 -0500

    Update main.cpp

--- practices/Blending/main.cpp
+++ practices/Blending/main.cpp
@@ -246,6 +246,8 @@ int main(int argc, char* argv[])
         ourShader.setMat4("view", view);
 
         // cubes
+        glEnable(GL_DEPTH_TEST);
+        glDisable(GL_BLEND);
         glBindVertexArray(cubeVAO);
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, cubeTexture);
@@ -263,6 +265,9 @@ int main(int argc, char* argv[])
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         // windows (from furthest to nearest)
+        glDisable(GL_DEPTH_TEST);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glBindVertexArray(transparentVAO);
         glBindTexture(GL_TEXTURE_2D, transparentTexture);
         for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); ++it)
