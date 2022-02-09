441b54bfe81f350b597f9ddf2df57a6b0ea40b8f
Muru Chen <muruc@andrew.cmu.edu>
2022-02-08 23:32:53 -0500

    update stb_image lib

--- practices/Materials/main.cpp
+++ practices/Materials/main.cpp
@@ -1,7 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 #include "shader.h"
 #include "camera.h"
 #include <iostream>
